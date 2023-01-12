7f8df4f85847eafe31588babdd138f97db4391a0
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2023-01-12 12:51:45 +0800

    changed

--- lesson/dataset.py
+++ lesson/dataset.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def get_beans(counts):
     xs = np.random.rand(counts)
-    xs = np.sort(xs)
+    # xs = np.sort(xs)
     ys = [1.7 * x + np.random.rand() / 5 for x in xs]
     # ys = [1.2 * x + np.random.rand() / 10 for x in xs]
     return xs, ys
