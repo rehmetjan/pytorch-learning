d4c8b9a15db76736c930c2a11a4a0a7b7dd6752c
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2023-01-12 13:36:02 +0800

    dont like the python import

--- lesson/lesson1/test.py
+++ lesson/lesson1/test.py
@@ -1,11 +1,23 @@
-import dataset
+# from lesson import dataset
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data
-xs, ys = dataset.get_beans(100)
+# xs, ys = lesson.dataset.get_beans(100)
+
+
+def get_beans(counts):
+    xs = np.random.rand(counts)
+    # xs = np.sort(xs)
+    ys = [1.7 * x + np.random.rand() / 5 for x in xs]
+    # ys = [1.2 * x + np.random.rand() / 10 for x in xs]
+    # ys = np.random.rand(counts)
+    return xs, ys
+
+
+xs, ys = get_beans(100)
 print(xs)
 print(ys)
 
--- test_data/dataset.py
+++ test_data/dataset.py
@@ -4,7 +4,7 @@ import numpy as np
 def get_beans(counts):
     xs = np.random.rand(counts)
     xs = np.sort(xs)
-    # ys = [1.7 * x + np.random.rand() / 5 for x in xs]
-    ys = [1.2 * x + np.random.rand() / 10 for x in xs]
+    ys = [1.7 * x + np.random.rand() / 5 for x in xs]
+    # ys = [1.2 * x + np.random.rand() / 10 for x in xs]
     # ys = np.random.rand(counts)
     return xs, ys
