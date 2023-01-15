15c8f90ab0f72a0aaba5436156824e1c37b60db3
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2023-01-15 22:33:30 +0800

    ddd

---
+++ function_math/login.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {*} param 
+ */
+function login (param) {
+    param.split("&")
+}
+
+// based on the login method get the parameters and if they are equal to given parameters return true
+function login (param) { 
+    var params = param.split("&")
+}
