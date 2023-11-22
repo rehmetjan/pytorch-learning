acebe59d957c5c0034d39ee5ef4b7d37f2b07260
Rehmetjan Tursun <rehmetjan.tursun@gmail.com>
2023-11-23 02:15:17 +0800

    xl

--- excel/test.py
+++ excel/test.py
@@ -1,14 +1,17 @@
+'''xlwt test'''
+
 import xlwt
+import openpyxl as xl
 
 wb = xlwt.Workbook()
 sheet = wb.add_sheet('Movies')
 sheet.write(0, 0, 'Html Movie Title')
 
-import openpyxl as xl
 
 wb = xl.load_workbook('movies.xlsx')
 sheet = wb['Sheet1']
 cell = sheet['a1']
 cell = sheet.cell(1, 1)
+
 # Log statement to print the value of cell
 print(cell.value)
