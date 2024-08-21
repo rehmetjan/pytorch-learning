6e75928eef9bdc703efbd6d2d33ddecea9289ed3
Rehmet <rehmetjan.tursun@gmail.com>
2024-08-22 06:44:41 +0800

    Update OPENAI API key in sample.py

--- openai/sample.py
+++ openai/sample.py
@@ -1,7 +1,7 @@
 """ChatGPG sample code using OPENAI API."""
 import openai
 
-openai.api_key = 'sk-eOg4nqGPuSDpHMmXMkUqT3BlbkFJOGnl7CdcGuXDICD7Ihca3'
+openai.api_key = 'sk-xxxxxx'  # 请替换为您的API Key
 
 response = openai.Completion.create(
     engine="text-davinci-003",  # 或者使用其他可用的引擎
